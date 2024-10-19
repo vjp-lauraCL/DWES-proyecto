f8dce64eadc305bd25da583671ce24c57b1b801e
vjp-lauraCL <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-10-19 15:39:31 +0200

    proyecto

--- utils/utils.php
+++ utils/utils.php
@@ -1,17 +1,26 @@
 <?php
-    function esOpcionMenuActiva(string $opcionMenu): bool{
-        if($_SERVER['REQUEST_URI']===$opcionMenu){
+    /**
+     * Verifica si una opción de menú está activa comparando con la URL actual.
+    
+     */
+    function esOpcionMenuActiva(string $opcionMenu): bool {
+        if ($_SERVER['REQUEST_URI'] === $opcionMenu) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-        function existeOpcionMenuActivaEnArray(array $opciones): bool {
-            foreach ($opciones as $opcion) {
-                if (esOpcionMenuActiva($opcion)) {
-                    return true;
-                }
+
+    /**
+     * Verifica si alguna de las opciones de menú en un array está activa.
+     
+     */
+    function existeOpcionMenuActivaEnArray(array $opciones): bool {
+        foreach ($opciones as $opcion) {
+            if (esOpcionMenuActiva($opcion)) {
+                return true;
             }
-            return false;
         }
+        return false;
+    }
 ?>
