63d8c36ddb0f3226079bed17a08b0dbf59296d40
Laura Casero Labrador <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-11-06 20:40:46 +0100

    app

--- entities/Connection.class.php
+++ entities/Connection.class.php
@@ -1,15 +1,17 @@
 <?php
+require_once 'app/config.php'
 class Connection{
 
-   public static function make(Array $config){
+   public static function make(){
    try{
+      $config = App::get('config')['database'];
     $connection = new PDO($config['conection']. ';dbname='.$config['name'],
     $config['username'],
     $config['password'],
     $config['options']);
 
    }catch(PDOException $PDOException){
-        die($PDOException->getMessage());
+       throw new AppException("No se ha podido crear conexion con la BD");
    }
    return $connection;
    }
